ecc53bf69f178691ca9d915bcd73cb5f58c985eb
aourzik <a.ourzik@gmail.com>
2025-12-18 10:37:10 +0100

    feat: create a function that prints something waiting for the user to enter a command

---
+++ readline.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    while (1)
+    {
+        printf("$ ");
+        read = getline(&line, &len, stdin);
+
+        if (read == -1)
+        {
+            printf("EOF Error");
+            break;
+        }
+    }
+
+    free(line);
+    return (0);
+}
